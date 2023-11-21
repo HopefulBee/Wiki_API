14008e43f07b2c834c9d55e61a869821cd527c49
HopefulBee <ijeomahmaria@gmail.com>
2023-11-21 12:09:45 +0100

    Using app.route()

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -8,11 +9,10 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-const mongoose = require('mongoose');
-
+//Mongoose Connection
 main().catch(err => console.log(err));
-
 async function main() {
+
     await mongoose.connect('mongodb://127.0.0.1:27017/WikiDB');
 
     const articlesSchema = new mongoose.Schema({
@@ -22,35 +22,32 @@ async function main() {
 
     const Article = mongoose.model('Article', articlesSchema);
 
-    app.get("/articles", async (req, res) => {
-        try {
-            await Article.find({})
-            .then( foundArticles => { res.send(foundArticles); });
-        } catch (error) {
-            console.log(error);
-        }
-    });
-
-    app.post("/articles", async (req, res) => {
-       const topic = new Article ({
-            title: req.body.title,
-            content: req.body.content
-       });
-
-       await topic.save();
-       res.send("Added!")
-    });
-
-    // Delete Request
-    app.delete("/articles", async (req, res) => {
-        try {
-            await Article.deleteMany()
-        .then(deletedArticles => { res.send("Successfully deleted")});
-        } catch (error) {
-            console.log(error);
-        }
-        
-    });
+    app.route("/articles")
+        .get(async (req, res) => {
+            try {
+                await Article.find({})
+                .then( foundArticles => { res.send(foundArticles); });
+            } catch (error) {
+                console.log(error);
+            }
+        })
+        .post(async (req, res) => {
+            const topic = new Article ({
+                 title: req.body.title,
+                 content: req.body.content
+            });
+     
+            await topic.save();
+            res.send("Added!")
+         })
+         
+        .delete(async (req, res) => {
+            try {
+                await Article.deleteMany().then(deletedArticles => { res.send("Successfully deleted")});
+            } catch (error) {
+                console.log(error);
+            }
+        });
 
     //Server Check
     app.listen(3000, () => {
