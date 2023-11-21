fbc3df88689f37bb5a6de4aca6de4b000cbd094e
HopefulBee <ijeomahmaria@gmail.com>
2023-11-21 14:32:54 +0100

    added .put() .patch()

--- app.js
+++ app.js
@@ -22,30 +22,58 @@ async function main() {
 
     const Article = mongoose.model('Article', articlesSchema);
 
+    ////////////////// Targetting All Articles//////////////////////////////////////
     app.route("/articles")
         .get(async (req, res) => {
             try {
                 await Article.find({})
-                .then( foundArticles => { res.send(foundArticles); });
+                    .then(foundArticles => { res.send(foundArticles); });
             } catch (error) {
                 console.log(error);
             }
         })
         .post(async (req, res) => {
-            const topic = new Article ({
-                 title: req.body.title,
-                 content: req.body.content
+            const topic = new Article({
+                title: req.body.title,
+                content: req.body.content
             });
-     
+
             // await topic.save();
             res.send("Added!")
-         })
+        })
         .delete(async (req, res) => {
             try {
-                await Article.deleteMany().then(deletedArticles => { res.send("Successfully deleted")});
+                await Article.deleteMany().then(deletedArticles => { res.send("Successfully deleted") });
             } catch (error) {
                 console.log(error);
             }
+        });
+
+    //////////////////Targetting Specific Articles//////////////////////////////////
+    app.route("/articles/:articleTitle")
+        .get(async (req, res) => {
+            await Article.findOne({ title: req.params.articleTitle })
+                .then(foundArticle => {
+                    if (foundArticle) {
+                        res.send(foundArticle);
+                    } else {
+                        res.send("NO MATCH FOUND");
+                    }
+                });
+        })
+
+        .put( async (req, res) => {
+            let query = req.params.articleTitle;
+            await Article.updateOne( 
+                {title: query},
+                {title: req.body.title, content: req.body.content},
+                {overwrite: true}).then(updatedArticle => {
+                    if (updatedArticle) {
+                        res.send("Updated Successfully");
+                    } else {
+                        res.send("No updates made")
+                    }
+                });
         });
 
     //Server Check
